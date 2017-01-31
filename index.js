f37c4a5ffd402051736aea28f641ac620d5557f2
Rich <ibanez19@sbcglobal.net>
2017-01-31 18:19:54 +0000

    Upload completed project files

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var app = express(); //Server
 var mongoose = require('mongoose'); //DB ops
-var config = require('./config');
 var port = process.env.PORT || 8080; //Set port
+var imageSearch = require('node-google-image-search'); // Google Search API
 
 //Connect to DB
-mongoose.connect(config.urlDb);
+mongoose.connect(process.env.DB);
 
 //Search Schema
 var SearchSchema = mongoose.Schema({
@@ -25,19 +26,21 @@ app.get('/index.html', function(req, res) {
     return res.redirect('/');
 });
 
+//Root displays 10 most recent results
 app.get('/', function(req, res) {
-    //Display 10 most recent searches
+    //Find the 10 most recent searches in DB
     searchEntry.find({}, '-_id term when').sort('-when').limit(10).exec(function(err, results) {
         if (err) console.error(err);
-        return res.json(results);
+        //Display results to user
+        return res.send(results);
     });
 });
-
 
 //Image Search route 
 app.get('/:searchTerm', function(req, res) {
     var searchTerm = req.params.searchTerm; //Get user's search term
-    var offset = req.query['offset'] || 10; //Get offset value; defaults to 10
+    //Get offset value, which represents page number. Defaults to 0
+    var offset = (req.query['offset'] - 1) * 10 || 0;
     //Get date and time of search
     var d = new Date();
     var searchTime = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
@@ -47,16 +50,29 @@ app.get('/:searchTerm', function(req, res) {
         when: searchTime
     });
     newSearch.save().then(function(newSearch) {
-        //TO DO: implement Google Image API here, display results
-        return res.send(`Search route: ${searchTerm} at ${searchTime}
-                        <br>Offset: ${offset}`);
+        var filteredResults = []; //New array to display filtered results
+        //Run Google Image Search API
+        imageSearch(searchTerm, displayResults, offset, 10);
+        
+        //Filter and display results
+        function displayResults(results) {
+            //Extract desired fields into filteredResults
+            results.forEach(function(obj) {
+                filteredResults.push({
+                    url: obj.link,
+                    snippet: obj.snippet,
+                    thumbnail: obj.image.thumbnailLink,
+                    context: obj.image.contextLink
+                });
+            });
+            //Display results to user
+            res.send(filteredResults);
+        }
     });
 });
-
-
 
 app.listen(port, function() {
     console.log('Server is listening on port ' + port);
 });
 
-exports.searchEntry = searchEntry; //For use in other files
+
