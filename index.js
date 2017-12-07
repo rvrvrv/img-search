780b329498fdde5a706beb5d867fda941e7b0d2d
rvrvrv <ibanez19@sbcglobal.net>
2017-12-07 13:27:42 -0600

    Implement ES6, landing page, and recent route

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
 require('dotenv').config();
-var express = require('express');
-var app = express(); //Server
-var mongoose = require('mongoose'); //DB ops
-var port = process.env.PORT || 8080; //Set port
-var imageSearch = require('node-google-image-search'); // Google Search API
+const express = require('express');
 
-//Connect to DB
+const app = express();
+const port = process.env.PORT || 8080;
+const mongoose = require('mongoose'); // DB ops
+const imageSearch = require('node-google-image-search'); // Google Search API
+const path = require('path');
+
+const indexPage = path.join(`${__dirname}/index.html`);
+
+// Connect to DB
 mongoose.connect(process.env.DB);
 
-//Search Schema
-var SearchSchema = mongoose.Schema({
-    term: String,
-    when: String
-});
-//Search Model
-var searchEntry = mongoose.model('searchEntry', SearchSchema);
-
-//Ignore favicon requests
-app.get('/favicon.ico', function(req, res) {
-    res.sendStatus(204);
+// Search Schema
+const SearchSchema = mongoose.Schema({
+  term: String,
+  when: String
 });
 
-//Landing page (redirects to /)
-app.get('/index.html', function(req, res) {
-    return res.redirect('/');
+// Search Model
+const SearchEntry = mongoose.model('searchEntry', SearchSchema);
+
+// Ignore favicon requests
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(204);
 });
 
-//Root displays 10 most recent results
-app.get('/', function(req, res) {
-    //Find the 10 most recent searches in DB
-    searchEntry.find({}, '-_id term when').sort('-when').limit(10).exec(function(err, results) {
-        if (err) console.error(err);
-        //Display results to user
-        return res.send(results);
-    });
+// Landing page
+app.get('/', (req, res) => res.sendFile(indexPage));
+app.get('/index.html', (req, res) => res.sendFile(indexPage)); // Disallow index.html as searchTerm param
+
+// Route for image search
+app.get('/:searchTerm', (req, res) => {
+  const searchTerm = req.params.searchTerm; // Get user's search term
+  // Get offset value, which represents page number. Defaults to 0
+  const offset = (req.query.offset - 1) * 10 || 0;
+  // Get date and time of search
+  const d = new Date();
+  const searchTime = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+  // Save search term and time in database
+  const newSearch = new SearchEntry({
+    term: searchTerm,
+    when: searchTime
+  });
+  newSearch.save().then(() => {
+    const filteredResults = []; // New array to display filtered results
+    // Run Google Image Search API
+    imageSearch(searchTerm, displayResults, offset, 10);
+
+    // Filter and display results
+    function displayResults(results) {
+      // Extract desired fields into filteredResults
+      results.forEach((obj) => {
+        filteredResults.push({
+          url: obj.link,
+          snippet: obj.snippet,
+          thumbnail: obj.image.thumbnailLink,
+          context: obj.image.contextLink
+        });
+      });
+      // Display results to user
+      res.send(filteredResults);
+    }
+  });
 });
 
-//Image Search route 
-app.get('/:searchTerm', function(req, res) {
-    var searchTerm = req.params.searchTerm; //Get user's search term
-    //Get offset value, which represents page number. Defaults to 0
-    var offset = (req.query['offset'] - 1) * 10 || 0;
-    //Get date and time of search
-    var d = new Date();
-    var searchTime = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
-    //Save search term and time in database
-    var newSearch = new searchEntry({
-        term: searchTerm,
-        when: searchTime
-    });
-    newSearch.save().then(function(newSearch) {
-        var filteredResults = []; //New array to display filtered results
-        //Run Google Image Search API
-        imageSearch(searchTerm, displayResults, offset, 10);
-        
-        //Filter and display results
-        function displayResults(results) {
-            //Extract desired fields into filteredResults
-            results.forEach(function(obj) {
-                filteredResults.push({
-                    url: obj.link,
-                    snippet: obj.snippet,
-                    thumbnail: obj.image.thumbnailLink,
-                    context: obj.image.contextLink
-                });
-            });
-            //Display results to user
-            res.send(filteredResults);
-        }
-    });
+// Route for most recent searches
+app.get('/api/recent', (req, res) => {
+  // Find the 10 most recent searches in DB
+  SearchEntry.find({}, '-_id term when').sort('-when').limit(10).exec((err, results) => {
+    if (err) console.error(err);
+    // Display results to user
+    return res.send(results);
+  });
 });
 
-app.listen(port, function() {
-    console.log('Server is listening on port ' + port);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
-
-
